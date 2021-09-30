700fef1da11ab88f8328c2bae6b5d479489d4766
imharshraj02 <91587576+imharshraj02@users.noreply.github.com>
2021-09-30 22:52:22 +0530

    Add files via upload

---
+++ Odd Even.py
@@ -0,0 +1,17 @@
+# Python Program to check if a Number is Odd or Even
+
+number = int(input("  Enter any Integer Value : "))
+
+if(number % 2 == 0):
+    print("{0} is an Even Number".format(number))
+else:
+    print("{0} is an Odd Number".format(number))# Python Program to check if a Number is Odd or Even
+
+number = int(input("  Enter any Integer Value : "))
+
+if(number % 2 == 0):
+    print("{0} is an Even Number".format(number))
+else:
+    print("{0} is an Odd Number".format(number))
+    
+    number = int(input("  Enter any Integer Value : "))
